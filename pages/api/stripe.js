70a6c97a78e3c33b03cfed8354387c8fd49abd6c
FathyMuhamed <fathimuhamed10522@gmail.com>
2022-06-24 15:22:32 +0200

    add env example

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
           }
         }),
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}/`,
       }
       const session = await stripe.checkout.sessions.create(params);
       res.status(200).json(session);
